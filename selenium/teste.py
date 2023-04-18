43dc56b186f6b4a0dd824ba66641bbf599a49917
Julio Lemos <60183908+lemos-julio@users.noreply.github.com>
2023-04-18 15:38:00 -0300

    Update teste.py

--- selenium/teste.py
+++ selenium/teste.py
@@ -14,7 +14,7 @@ driver = webdriver.Chrome(service=service, options=options)
 def testBrowser():
     try:
         driver.get('https://www.selenium.dev/selenium/web/web-form.html')
-        driver.find_element(By.ID,'my-text-id').send_keys('teste')
+        driver.find_element(By.ID,'my-text').send_keys('teste')
     except ValueError:
         print(ValueError())
 
