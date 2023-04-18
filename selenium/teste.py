8054c11ae5b4584b26eaf8eb7870268f14910937
Julio Lemos <julio2012lemos@gmail.com>
2023-04-18 10:26:52 -0300

    Added initial test in selenium

---
+++ selenium/teste.py
@@ -0,0 +1,21 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+options =  webdriver.ChromeOptions()
+
+service = ChromeService(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+
+def testBrowser():
+    try:
+        driver.get('http://localhost:3000/')
+        driver.find_element(By.ID,'my-text-id').send_keys('teste')
+    except ValueError:
+        print(ValueError())
+
+testBrowser()
